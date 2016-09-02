e07e56067ede78354698d72b6b876236cc01a71c
Jon Allured <jon.allured@gmail.com>
2016-09-02 14:40:39 -0500

    Reorganize config

--- config.rb
+++ config.rb
@@ -3,17 +3,17 @@ page '/posts/*', layout: :post
 page 'atom.xml', layout: false
 page '/jack/*', layout: false
 
-activate :blog do |blog|
-  blog.sources = '{year}-{month}-{day}-{title}'
-  blog.prefix = 'posts'
-  blog.default_extension = '.md'
-end
-
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'images'
 
 set :markdown_engine, :redcarpet
+
+activate :blog do |blog|
+  blog.sources = '{year}-{month}-{day}-{title}'
+  blog.prefix = 'posts'
+  blog.default_extension = '.md'
+end
 
 activate :google_analytics do |ga|
   ga.tracking_id = 'UA-3137727-1'
