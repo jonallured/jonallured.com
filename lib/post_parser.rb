af12984d34d259733a09487b0e0ea3f74b8e8aec
Jon Allured <jon.allured@gmail.com>
2021-01-31 14:59:16 -0600

    Improve social share image generation

--- lib/post_parser.rb
+++ lib/post_parser.rb
@@ -4,35 +4,143 @@ require 'yaml'
 require 'date'
 require 'active_support/all'
 
-class PostParser
-  AVERAGE_WORDS_PER_MINUTE = 180
-
-  def self.read(path)
-    data = File.read(path)
-    _, yaml, body = data.split('---')
-    front_matter = YAML.safe_load(yaml)
-    word_count = body.split.size
-
-    compute_options(path, word_count, front_matter)
+class TitleParser
+  def self.parse(title)
+    new(title).parse
   end
 
-  def self.compute_options(path, word_count, front_matter)
-    id = front_matter['id']
-    reading_time = (word_count / AVERAGE_WORDS_PER_MINUTE.to_f).ceil
+  attr_reader :title
 
+  def initialize(title)
+    @title = title
+  end
+
+  def parse
+    if title.include?(';')
+      parse_with_semi
+    else
+      simple_parse
+    end
+  end
+
+  private
+
+  def group_title_words
+    words = title.split
+
+    if words.length > 5 && words.length < 10
+      words.in_groups(2, false)
+    else
+      words.in_groups_of(5, false)
+    end
+  end
+
+  def clean_parts(grouped)
+    if grouped.count > 2
+      first, last = grouped.take(2)
+      cutoff = last.length + 2
+      [first, maybe_snip_part(last, cutoff, force: true)]
+    else
+      cutoff = 35
+
+      grouped.map do |part|
+        maybe_snip_part(part, cutoff)
+      end
+    end
+  end
+
+  def simple_parse
+    word_groups = group_title_words
+    grouped = word_groups.map { |group| group.join(' ') }
+    clean_parts(grouped)
+  end
+
+  def parse_with_semi
+    first, last = title.split('; ').take(2)
+    first << ';'
+    parts = [first, last]
+    cutoff = 31
+
+    parts.map do |part|
+      maybe_snip_part(part, cutoff)
+    end
+  end
+
+  def maybe_snip_part(part, cutoff, force: false)
+    return part unless part.length > cutoff || force
+
+    punctuation_list = [',', ';', '.']
+    offset = punctuation_list.include?(part.last) ? 4 : 3
+    end_index = cutoff - offset
+    suffix = part.last == ';' ? ';' : ''
+
+    "#{part[0..end_index].strip}...#{suffix}"
+  end
+end
+
+class PostOptions
+  AVERAGE_WORDS_PER_MINUTE = 180
+
+  attr_reader :path, :front_matter, :body
+
+  def self.as_hash(path, front_matter, body)
+    new(path, front_matter, body).as_hash
+  end
+
+  def initialize(path, front_matter, body)
+    @path = path
+    @front_matter = front_matter
+    @body = body
+  end
+
+  def as_hash
     {
-      output_path: "source/images/post-#{id}/social-share.png",
-      published_at: Date.parse(path[13, 10]),
+      output_path: output_path,
+      published_at: published_at,
       reading_time: reading_time,
-      shrt_url: "jon.zone/post-#{id}",
-      title_parts: compute_title_parts(front_matter['title']),
+      shrt_url: shrt_url,
+      title_parts: title_parts,
       word_count: word_count
     }
   end
 
-  def self.compute_title_parts(title)
-    split_up = title.split
-    grouped = split_up.in_groups_of(5, false)
-    grouped.map { |group| group.join(' ') }
+  private
+
+  def id
+    front_matter['id']
+  end
+
+  def output_path
+    "source/images/post-#{id}/social-share.png"
+  end
+
+  def published_at
+    Date.parse(path[13, 10])
+  end
+
+  def reading_time
+    (word_count / AVERAGE_WORDS_PER_MINUTE.to_f).ceil
+  end
+
+  def shrt_url
+    "jon.zone/post-#{id}"
+  end
+
+  def title_parts
+    TitleParser.parse(front_matter['title'])
+  end
+
+  def word_count
+    body.split.size
+  end
+end
+
+class PostParser
+  def self.read(path)
+    data = File.read(path)
+    _, yaml, body = data.split('---')
+    front_matter = YAML.safe_load(yaml)
+
+    PostOptions.as_hash(path, front_matter, body)
   end
 end
--- spec/lib/post_parser_spec.rb
+++ spec/lib/post_parser_spec.rb
@@ -4,32 +4,126 @@ require_relative '../../lib/post_parser'
 
 RSpec.describe PostParser do
   describe '.read' do
-    it 'returns options' do
-      path = 'source/posts/2016-09-16-adding-additional-virtual-hosts-to-a-digital-ocean-server.html.md'
-      options = PostParser.read(path)
+    context 'with a single line title' do
+      it 'returns standard options' do
+        options = PostParser.read(
+          'source/posts/2020-12-23-dealing-with-rotten-links.html.md'
+        )
 
-      expect(options).to eq(
-        output_path: 'source/images/post-40/social-share.png',
-        published_at: Date.parse('2016-09-16'),
-        reading_time: 2,
-        shrt_url: 'jon.zone/post-40',
-        title_parts: ['Adding Additional Virtual Hosts to', 'a Digital Ocean Server'],
-        word_count: 185
-      )
+        expect(options).to eq(
+          output_path: 'source/images/post-41/social-share.png',
+          published_at: Date.parse('2020-12-23'),
+          reading_time: 3,
+          shrt_url: 'jon.zone/post-41',
+          title_parts: ['Dealing With Rotten Links'],
+          word_count: 524
+        )
+      end
     end
 
-    it 'returns options' do
-      path = 'source/posts/2020-12-23-dealing-with-rotten-links.html.md'
-      options = PostParser.read(path)
+    context 'with a double line title' do
+      it 'returns options with two title parts' do
+        options = PostParser.read(
+          'source/posts/2016-09-16-adding-additional-virtual-hosts-to-a-digital-ocean-server.html.md'
+        )
 
-      expect(options).to eq(
-        output_path: 'source/images/post-41/social-share.png',
-        published_at: Date.parse('2020-12-23'),
-        reading_time: 3,
-        shrt_url: 'jon.zone/post-41',
-        title_parts: ['Dealing With Rotten Links'],
-        word_count: 524
-      )
+        expect(options[:title_parts]).to eq(
+          [
+            'Adding Additional Virtual Hosts to',
+            'a Digital Ocean Server'
+          ]
+        )
+      end
+    end
+
+    context 'with a title that has a semi colon' do
+      it 'returns options with the title parts split on that semi colon' do
+        options = PostParser.read(
+          'source/posts/2012-01-31-the-simpleton-pattern-when-to-include-when-to-extend.html.md'
+        )
+
+        expect(options[:title_parts]).to eq(
+          [
+            'The Simpleton Pattern;',
+            'When to Include, When to Extend'
+          ]
+        )
+      end
+    end
+
+    context 'with a title that has a semi and is too long' do
+      it 'returns options with a last title part thats been snipped' do
+        options = PostParser.read(
+          'source/posts/2012-01-12-delayed-job-hits-3-marston-on-deprecating-legacy-code.html.md'
+        )
+
+        expect(options[:title_parts]).to eq(
+          [
+            'Delayed Job Hits 3.0;',
+            'Marston on Deprecating Legacy...'
+          ]
+        )
+      end
+    end
+
+    context 'with a title that has a semi colon and the first part is too long' do
+      it 'returns options with title parts that include a first part that has been snipped' do
+        options = PostParser.read(
+          'source/posts/2011-12-19-multiple-carets-in-textmate-2-default-scope.html.md'
+        )
+
+        expect(options[:title_parts]).to eq(
+          [
+            'Multiple Carets in TextMate...;',
+            'Default Scope'
+          ]
+        )
+      end
+    end
+
+    context 'with a really long title' do
+      it 'returns options with a final title part that has been snipped' do
+        options = PostParser.read(
+          'source/posts/2011-08-08-keeping-a-live-website-in-sync-with-a-local-version-part-one.html.md'
+        )
+
+        expect(options[:title_parts]).to eq(
+          [
+            'Keeping a Live Website in',
+            'Sync With a Local Version...'
+          ]
+        )
+      end
+    end
+
+    context 'with a medium title' do
+      it 'returns options with two title parts' do
+        options = PostParser.read(
+          'source/posts/2011-11-28-playing-around-with-content-editable-in-rails.html.md'
+        )
+
+        expect(options[:title_parts]).to eq(
+          [
+            'Playing Around With Content',
+            'Editable in Rails'
+          ]
+        )
+      end
+    end
+
+    context 'with a long title ending in a period' do
+      it 'returns options that have a final title part with that trailing period removed' do
+        options = PostParser.read(
+          'source/posts/2012-02-06-klabnik-on-moving-from-sinatra-to-rails-and-acceptance-vs-integration-tests.html.md'
+        )
+
+        expect(options[:title_parts]).to eq(
+          [
+            'Klabnik on Moving From Sinatra',
+            'to Rails and Acceptance vs...'
+          ]
+        )
+      end
     end
   end
 end
