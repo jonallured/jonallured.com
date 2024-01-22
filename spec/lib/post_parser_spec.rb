9e624fd85611f3ec25df5caae7607d33c66bff25
Jon Allured <jon@jonallured.com>
2024-01-22 13:30:22 -0600

    Remove frozen string comment

--- spec/lib/post_parser_spec.rb
+++ spec/lib/post_parser_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "../../lib/post_parser"
 
 RSpec.describe PostParser do
--- spec/lib/title_parser_spec.rb
+++ spec/lib/title_parser_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "../../lib/title_parser"
 
 RSpec.describe TitleParser do
