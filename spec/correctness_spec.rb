01ae811f2fc6943b4d0142655cfb8355ec041304
Jon Allured <jon@jonallured.com>
2024-11-26 14:41:43 -0600

    Add spec to assess migration progress

---
+++ spec/correctness_spec.rb
@@ -0,0 +1,14 @@
+describe "correctness" do
+  it "builds the same number of files" do
+    fd_command = 'fd . build --exclude "posts" --exclude "images/post*" --type file'
+
+    mm_output = `cd ../mm-jonallured.com/ && #{fd_command}`
+    mm_matches = mm_output.split("\n")
+
+    system "rm -rf build && bundle exec jekyll build"
+    jekyll_output = `#{fd_command}`
+    jekyll_matches = jekyll_output.split("\n")
+
+    expect(jekyll_matches).to match_array mm_matches
+  end
+end
